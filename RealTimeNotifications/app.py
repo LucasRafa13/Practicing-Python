afee66bfc8506135f2885c5fcae3b9c988038869
Lucas Lima <lucasrafael123araujolima@gmail.com>
2025-04-27 12:17:13 -0400

    feat: :sparkles: Pix Payments With Flask

---
+++ RealTimeNotifications/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from repository.database import db
+from db.payment import Payment
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = "SECRET_KEY_WEBSOCKET"
+
+db.init_app(app)
+
+
+@app.route("/payments/pix", methods=["POST"])
+def create_payment_pix():
+    return jsonify({"message": " Payment created successfully"}), 201
+
+
+@app.route("/payments/pix/confirmation", methods=["POST"])
+def pix_confirmation():
+    return jsonify({"message": " Payment confirmed successfully"}), 200
+
+
+@app.route("/payments/pix/<int:payment_id>", methods=["GET"])
+def payment_pix_page(payment_id):
+    return "Pagamento Pix"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
